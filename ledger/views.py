8434262625f6b43c7ff29f6269b1dd51fd8f2a27
Sticity <119797972+Sticity@users.noreply.github.com>
2025-02-24 22:12:41 +0800

    Added view in ledger/views.py
    Specifically for the page, Recipe 1

--- ledger/views.py
+++ ledger/views.py
@@ -1,3 +1,33 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
 
 # Create your views here.
+def recipe1(request):
+    ctx = {
+        "name": "Recipe 1",
+        "ingredients": [
+            {
+                "name": "tomato",
+                "quantity": "3pcs"
+            },
+            {
+                "name": "onion",
+                "quantity": "1pc"
+            },
+            {
+                "name": "pork",
+                "quantity": "1kg"
+            },
+            {
+                "name": "water",
+                "quantity": "1L"
+            },
+            {
+                "name": "sinigang mix",
+                "quantity": "1 packet"
+            }
+        ],
+        "link": "/recipe/1"
+    }
+    return render(request, 'base.html', ctx)
