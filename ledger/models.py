96fdd09b2ff0ead8cb84e57104ba01fcae57dec7
Sticity <119797972+Sticity@users.noreply.github.com>
2025-02-27 10:35:22 +0800

    Created the models, Ingredient, Recipe, and RecipeIngredient

--- ledger/models.py
+++ ledger/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Ingredient(models.Model):
+    name = models.CharField(max_length=50)
+
+class Recipe(models.Model):
+    name = models.CharField(max_length=50)
+
+class RecipeIngredient(models.Model):
+    quantity = models.IntegerField()
+    ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
