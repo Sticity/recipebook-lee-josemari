0e542eb06e70891703d116091ab120d6d12f43db
Sticity <119797972+Sticity@users.noreply.github.com>
2025-02-28 16:46:17 +0800

    Simplified 'recipe1' and 'recipe2' into a single function

--- ledger/views.py
+++ ledger/views.py
@@ -4,13 +4,8 @@ from django.template import loader
 from .models import Ingredient, Recipe, RecipeIngredient
 
 # Create your views here.
-def recipe1(request):
-    rcp = Recipe.objects.get(pk=1)
-    ingredients = RecipeIngredient.objects.filter(recipe=rcp, ingredient__recipe__recipe__name=rcp.name)
-    return render(request, 'recipe.html', {'recipe':rcp, 'ingredients':ingredients})
-
-def recipe2(request):
-    rcp = Recipe.objects.get(pk=2)
+def recipe(request, num=1):
+    rcp = Recipe.objects.get(pk=num)
     ingredients = RecipeIngredient.objects.filter(recipe=rcp, ingredient__recipe__recipe__name=rcp.name)
     return render(request, 'recipe.html', {'recipe':rcp, 'ingredients':ingredients})
 
