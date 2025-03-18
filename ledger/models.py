064a0eed2d51e2252dc491bae0bbd91fc1b0b447
Sticity <119797972+Sticity@users.noreply.github.com>
2025-03-18 14:50:32 +0800

    Added 'Profile' model + Updated 'Recipe' model
    Added string representations for the necessary models.
    
    Recipe model:
    + author field
    + created_on field
    + updated_on field

--- ledger/models.py
+++ ledger/models.py
@@ -1,12 +1,30 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+    bio = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
 class Ingredient(models.Model):
     name = models.CharField(max_length=50)
 
+    def __str__(self):
+        return self.name
+
 class Recipe(models.Model):
     name = models.CharField(max_length=50)
-    url = models.CharField(max_length=50)
+
+    author = models.ForeignKey(Profile, on_delete=models.CASCADE, null=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return self.name
 
 class RecipeIngredient(models.Model):
     quantity = models.CharField(max_length=50)
