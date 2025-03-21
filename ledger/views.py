9c10f50f16adc6a9b2b32434c222976379207005
Sticity <119797972+Sticity@users.noreply.github.com>
2025-03-21 18:39:47 +0800

    Renamed variables in views.py

--- ledger/views.py
+++ ledger/views.py
@@ -5,14 +5,14 @@ from django.contrib.auth.decorators import login_required
 from .models import Ingredient, Recipe, RecipeIngredient
 
 # Create your views here.
-
 @login_required
 def recipe(request, num=1):
-    rcp = Recipe.objects.get(pk=num)
-    ingredients = RecipeIngredient.objects.filter(recipe=rcp, ingredient__recipe__recipe__name=rcp.name)
-    return render(request, 'recipe.html', {'recipe':rcp, 'ingredients':ingredients})
+    recipe = Recipe.objects.get(pk=num)
+    recipes = Recipe.objects.all()
+    ingredients = RecipeIngredient.objects.filter(recipe=recipe, ingredient__recipe__recipe__name=recipe.name)
+    return render(request, 'recipe.html', {'recipe':recipe, 'ingredients':ingredients, 'recipes':recipes})
 
 @login_required
 def recipe_list_context(request):
-    rcps = Recipe.objects.all()
-    return render(request, 'recipes_list.html', {'recipes':rcps})
+    recipes = Recipe.objects.all()
+    return render(request, 'recipes_list.html', {'recipes':recipes})
