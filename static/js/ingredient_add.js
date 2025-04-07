aea5125de76e1f6a9234b25f05de3c4890b1ce96
Sticity <119797972+Sticity@users.noreply.github.com>
2025-04-07 21:38:46 +0800

    Renamed function in ingredient_add.js

--- static/js/ingredient_add.js
+++ static/js/ingredient_add.js
@@ -1,4 +1,4 @@
-function addIngredient() {
+function add_ingredient() {
     const container = document.getElementById('ingredients');
     const block = container.firstElementChild.cloneNode(true);
     container.appendChild(block);
