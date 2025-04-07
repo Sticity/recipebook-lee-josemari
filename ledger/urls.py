2b8896bdd1cab8b44f86e7fcd1247a7ee3ed8889
Sticity <119797972+Sticity@users.noreply.github.com>
2025-04-07 20:42:22 +0800

    Updated urls.py

--- ledger/urls.py
+++ ledger/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import recipe, recipe_list_context
+from .views import recipe, recipe_list_context, recipe_add, add_recipe_image
 
 urlpatterns = [
-    path('recipe/<int:num>/', recipe, name="recipe"),
-    path('recipes/list/', recipe_list_context, name="recipe_list_context"),
+    path('recipe/<int:num>/', recipe, name='recipe'),
+    path('recipes/list/', recipe_list_context, name='recipe_list_context'),
+    path('recipe/add/', recipe_add, name='recipe_add'),
+    path('recipe/<int:pk>/add_image/', add_recipe_image, name='add_recipe_image'),
 ]
 
-app_name = "ledger"
+app_name = 'ledger'
